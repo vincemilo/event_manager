db78ef0f3c20a4e36fe35e0c1c7798977ce879a0
vincemilo <vincemilo@gmail.com>
2021-09-12 11:28:15 -0600

    Add to github

---
+++ lib/event_manager.rb
@@ -0,0 +1,72 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def clean_phone(phone)
+  phone.gsub!(/[^\d]/, '')
+  if phone.length == 10
+    phone
+  elsif phone.length == 11 && phone[0] == '1'
+    phone[1..10]
+  end
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+pop_hours = []
+pop_days = []
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+  form_letter = erb_template.result(binding)
+  reg_date = row[:regdate]
+  day = Time.strptime(reg_date, '%m/%d/%y %k:%M')
+  pop_hours.push(day.hour)
+  pop_days.push(day.strftime('%A'))
+  phone = clean_phone(row[:homephone])
+  save_thank_you_letter(id, form_letter)
+end
+
+puts "The most popular registration times were #{pop_hours.tally} and days were
+#{pop_days.tally}."
---
+++ lib/test.rb
@@ -0,0 +1,9 @@
+require 'erb'
+
+meaning_of_life = 42
+
+question = "The Answer to the Ultimate Question of Life, the Universe, and Everything is <%= meaning_of_life %>"
+template = ERB.new question
+
+results = template.result(binding)
+puts results
